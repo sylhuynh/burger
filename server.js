9eed726a6ad715c3b76b96fbe31b19e167b58a5d
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-04-17 01:10:56 -0700

    refractor: extract GET to controller

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const homeControllerRoutes = require("./controllers/homeController")
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -7,9 +8,7 @@ const PORT = process.env.PORT || 8080;
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => {
-    res.render("index");
-});
+app.use(homeControllerRoutes)
 
 app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:" + PORT);
