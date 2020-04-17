e108a7c068affc1dde91703d3bb7ed215b9ffdc6
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-04-17 01:53:29 -0700

    pretty print json on index handlebar

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const burgers = require("../models/burgers")
 
 router.get("/", (req, res) => {
-    res.render("index");
+    burgers.all((burgers) => {
+        const templateData = {burgers: JSON.stringify(burgers, null, 2)};
+
+        res.render("index", templateData);
+    });
 });
 
 module.exports = router;
