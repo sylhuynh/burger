c6748dc2bfb4458d784579392a3221e59b887695
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-04-17 21:54:15 -0700

    added create method to burgers model

--- models/burgers.js
+++ models/burgers.js
@@ -1,14 +1,15 @@
 const orm = require("../config/orm");
 
 const burger = {
-    all: (cb) => orm.getAll("burgers", cb)
+    all: (cb) => orm.getAll("burgers", cb),
+    create: (burger_name, cb) => orm.createOne("burgers", {burger_name}, cb)
 };
 
 module.exports = burger;
 
 // burger.all(burgers => console.log(burgers));
 
-// [
+// 
 //     RowDataPacket {
 //       id: 1,
 //       burger_name: 'Portabella Charburger',
@@ -17,3 +18,15 @@ module.exports = burger;
 //     RowDataPacket { id: 2, burger_name: 'Double-Double', devoured: 0 },
 //     RowDataPacket { id: 3, burger_name: 'Whopper', devoured: 0 }
 //   ]
+
+// burger.create("Impossible Burger", (result) => console.log(result));
+// OkPacket {
+//     fieldCount: 0,
+//     affectedRows: 1,
+//     insertId: 9,
+//     serverStatus: 2,
+//     warningCount: 0,
+//     message: '',
+//     protocol41: true,
+//     changedRows: 0
+//   }
