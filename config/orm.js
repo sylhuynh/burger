ea03f352e25fb2d4decb2f3a13a5702820259b98
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-04-17 21:44:02 -0700

    added createOne method to orm

--- config/orm.js
+++ config/orm.js
@@ -3,11 +3,23 @@ const connection = require("./connection");
 const orm = {
     getAll: (table, cb) => {
         connection.query("SELECT * FROM ??", [table], (err, data) => {
+            if (err) {
+                throw err;
+            }
             cb(data);
-        })
-    }
+        });
+    },
+    createOne: (table, newRowValues, cb) => {
+        const queryString = "INSERT INTO ?? SET ? "
+        connection.query(queryString, [table, newRowValues], (err, data) => {
+            if (err) {
+                throw err;
+            }
+            cb(data)
+        });
+    },
 
-}
+};
 
 module.exports = orm;
 
@@ -18,3 +30,18 @@ module.exports = orm;
 //     RowDataPacket { id: 2, burger_name: 'Double-Double', devoured: 0 },
 //     RowDataPacket { id: 3, burger_name: 'Whopper', devoured: 0 }
 // ]
+
+// orm.createOne("burgers", {burger_name: "Impossible Burger"}, result => {
+//     console.log(result);
+// });
+
+// OkPacket {
+//     fieldCount: 0,
+//     affectedRows: 1,
+//     insertId: 8,
+//     serverStatus: 2,
+//     warningCount: 0,
+//     message: '',
+//     protocol41: true,
+//     changedRows: 0
+//   }
